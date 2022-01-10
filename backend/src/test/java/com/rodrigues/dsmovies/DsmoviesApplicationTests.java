aa3b75fbe7547b9117f763020dc95f240367c289
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-01-10 18:08:20 -0300

    Project created

---
+++ backend/src/test/java/com/rodrigues/dsmovies/DsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.dsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
